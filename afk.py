9e4270acdcfa7e283584b66166e49d4b20c37456
Rus3311 <70165313+Rus3311@users.noreply.github.com>
2020-08-30 07:17:41 +0500

    Update afk.py

--- afk.py
+++ afk.py
@@ -16,7 +16,7 @@ class AFKMod(loader.Module):
                "gone": "Я выхожу с сети",
                "back": "Я с сети",
                "afk": "<b>(Автоответчик) Я не в сети (уже {}).</b>",
-               "afk_reason": "<b>(Автоответчик) Я не в сети (уже {}). "{}"}
+               "afk_reason": "{}"}
     async def client_ready(self, client, db):
         self._db = db
         self._me = await client.get_me()
